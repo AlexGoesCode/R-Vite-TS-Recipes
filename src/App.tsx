63ca8757970812d4cc64a9e5f881f86916ee99bb
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-05-17 14:00:50 +0200

    added Router
    added Router to existing files

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,42 @@ import './App.css';
 import SearchRecipe from './pages/SearchRecipe';
 import NavScrollExample from './components/navbar/Navbar';
 import Grid from './components/grid/Grid';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path='recipes' element={<Grid />} />
+        <Route path='search' element={<SearchRecipe />} />
+      </Route>
+    )
+  );
+
   return (
-    <Router>
-      <div>
-        <NavScrollExample />
-        <h1>Recipes</h1>
-        <Routes>
-          <Route path="/" element={<Grid />} />
-          <Route path="/search" element={<SearchRecipe />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      {/* <NavScrollExample /> */}
+      <h1>Recipes</h1>
+      <RouterProvider router={router} />
+    </div>
   );
 };
 
+const Root = () => {
+  return (
+    <>
+      <NavScrollExample />
+      <Outlet />
+    </>
+  );
+};
 export default App;
