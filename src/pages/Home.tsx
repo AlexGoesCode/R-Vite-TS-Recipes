b8b5ca0aac6bd42f437c971c1772b1986132273d
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-05-21 15:48:17 +0200

    Context and Provider in my app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import { useAppContext } from '../context/AppContext';
 
-function Home() {
+const Home = () => {
+  const { user, setUser } = useAppContext();
+
   return (
     <div>
-      <h1>Welcome to my website</h1>
+      <h1>Welcome {user ? user : 'Guest'}</h1>
+      <button onClick={() => setUser('John Doe')}>Login as John Doe</button>
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
-}
+};
 
 export default Home;
