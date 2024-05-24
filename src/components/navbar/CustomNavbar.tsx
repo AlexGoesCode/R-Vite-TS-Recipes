47087ce7964627c90c5dea205288b6141223daec
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-05-24 13:27:39 +0200

    Button link properly rooted

--- src/components/navbar/CustomNavbar.tsx
+++ src/components/navbar/CustomNavbar.tsx
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import ButtonLink from '../button-link/ButtonLink'; // Import the custom ButtonLink component
+import ButtonLink from '../../components/custom-link/ButtonLink'; // Import the custom ButtonLink component
 import './CustomNavbar.css';
 
 const CustomNavbar = () => {
