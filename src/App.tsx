b37d7d5108f7767fd751ec04aa57f0f3baca0b33
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-05-27 12:40:53 +0200

    + Recipes, - Grid, - SearchRecipes, + 2nd API key

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './App.css';
-import SearchRecipe from './pages/search-recipe/SearchRecipe';
 import CustomNavbar from './components/custom-navbar/CustomNavbar';
-import Grid from './components/grid/Grid';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -9,6 +7,7 @@ import Logout from './pages/logout/Logout';
 import SignUp from './pages/sign-up/SignUp';
 import NotFound from './pages/not-found/NotFound';
 import { AppProvider } from './context/AppContext';
+import Recipes from './pages/recipes/Recipes';
 
 const App = () => {
   return (
@@ -18,8 +17,7 @@ const App = () => {
         <div className='app'>
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='recipes' element={<Grid />} />
-            <Route path='search' element={<SearchRecipe />} />
+            <Route path='recipes' element={<Recipes />} />
             <Route path='/login' element={<Login />} />
             <Route path='/logout' element={<Logout />} />
             <Route path='/signup' element={<SignUp />} />
